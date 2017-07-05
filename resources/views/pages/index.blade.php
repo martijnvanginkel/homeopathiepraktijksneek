7d11e7e7dbaf180f2c8f6ad3297820c9fae8a4d5
Martijn van Ginkel <Martijn@MacBook-Pro-van-Martijn.local>
2017-07-05 13:27:32 +0200

    -

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -15,12 +15,12 @@
   <p>{!! nl2br(e($index->paragraphOne)) !!}</p>
 
   <div class="row">
-    <div class="col-lg-5">
+    <div class="col-lg-4">
       <div id="selfieContainer">
         <img src="{{URL::asset('storage/indexes/index2.jpg')}}" id="selfie"/>
       </div>
     </div>
-    <div class="col-lg-7">
+    <div class="col-lg-8">
       <h1 class="mainHeader">{{$index->titleTwo}}</h1>
         <p>{!! nl2br(e($index->paragraphTwo)) !!}</p>
     </div>
